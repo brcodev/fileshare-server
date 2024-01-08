22649362232ad880ad34f0d161eb55c7c18be3f5
Luis Bravo <107825960+brcodev@users.noreply.github.com>
2024-01-08 16:34:39 -0300

    update linkController.js

--- controllers/linkController.js
+++ controllers/linkController.js
@@ -53,7 +53,7 @@ exports.nuevoEnlace = async (req, res, next) => {
             enlace.nombre = req.file.filename;
             enlace.nombre_original = req.file.originalname;
 
-           /* if (req.user) {
+            if (req.user) {
                 
                 const { password, descargas } = req.body
                 enlace.password = password;
@@ -73,9 +73,9 @@ exports.nuevoEnlace = async (req, res, next) => {
 
                 enlace.autor = req.user.id;
 
-            } */
+            } 
 
-             if (req.user) {
+            /* if (req.user) {
                 
                 const password = req.file.password;
                 const descargas = req.file.descargas;
@@ -96,7 +96,9 @@ exports.nuevoEnlace = async (req, res, next) => {
 
                 enlace.autor = req.user.id;
 
-            } 
+            }
+
+            */
 
             try {
                 await enlace.save();
